9a03b371025e0d3804e4042739db2d263e3f1b23
ZobiaShakil <zobia.shakil1@gmail.com>
2024-08-22 20:38:49 +0500

    Add files via upload

---
+++ randomNoGenerator/script.js
@@ -0,0 +1,12 @@
+
+const generateBtn = document.getElementById("generate-btn");
+const numberElement = document.getElementById
+("random-number");
+
+
+generateBtn.addEventListener("click",()=>{
+    const randomNumber = Math.floor(Math.random() * 1000) +
+     1;
+
+    numberElement.textContent = randomNumber;
+});
